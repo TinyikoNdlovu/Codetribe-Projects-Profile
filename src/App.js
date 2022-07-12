b4729480d938601abc34be6c0ce842bf636063b0
codexpression-nj <n.codexpression@gmail.com>
2022-07-12 14:40:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Projects from './Projects/Project';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+<div id="app__works" className="app__container work">
+<div className="app__wrapper app__flex">
+<Projects  ></Projects>
+
+            </div>
+</div>
     </div>
+
   );
 }
 
---
+++ src/Projects/ProjectData.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import newsappImg from '../assets/image1.jpg'
+import weatherappImg from '../assets/image2.png'
+import planetImage from '../assets/image3.png'
+import onBoardImg from '../assets/image4.png'
+const ProjectData = [
+    {
+        title: "Todo App",
+        description: "A fullstack Todo list app running on mobile",
+        codeLink: "https://github.com/codexpression-nj/toDo-ionic5",
+        projectLink: "https://www.canva.com/design/DAFGIxaE6uk/F34KvhF-VkEaWJhECspfzQ/view?website#2",
+        imageUrl: newsappImg,
+        tag: "Ionic",
+     },
+     {
+        title: " Wather App",
+        description: "A weather mobile app collecting data from API request call",
+        codeLink: "https://github.com/codexpression-nj/weatherApp-Ionic6",
+        projectLink: "",
+        imageUrl: weatherappImg,
+        tag: "Ionic",
+     },
+     {
+        title: " OnBoard planet app",
+        description: "A mobile app implementing flutter swipe package ",
+        codeLink: "https://github.com/codexpression-nj/liquid_swipe_tutorial",
+        projectLink: "https://www.canva.com/design/DAFGId4dvwg/trc7GEPBIMczlA8ykPvIfw/view?website#1",
+        imageUrl: planetImage,
+        tag: "Flutter",
+     },{
+        title: "Food OnBoarding app",
+        description: "A food onBoading app with flutter animations",
+        codeLink: "https://github.com/codexpression-nj/minimal_food_app",
+        projectLink: "https://www.canva.com/design/DAFGIbpTpSY/t1UUMPmSKqHHpXOD1yD3Ug/view?website#4",
+        imageUrl: onBoardImg,
+        tag: "Flutter",
+     },
+]
+   
+
+
+export default ProjectData
