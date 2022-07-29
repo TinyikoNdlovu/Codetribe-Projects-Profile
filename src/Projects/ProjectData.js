e2067fa7804cd670f36b55989bf2352b44141511
techdevcodeweb <tinyiko13@yahoo.com>
2022-07-29 15:45:23 +0200

    Task app added

--- src/Projects/ProjectData.js
+++ src/Projects/ProjectData.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import sunnysideWebsiteImg from '../assets/image1.png'
-import budgetappImg from '../assets/image2.png'
-import covid19TrackerImg from '../assets/image3.png'
-import employeePortalImg from '../assets/image4.png'
-import empImg from '../assets/image5.png'
+import portfolioWebsiteImg from '../assets/image1.png'
+import sunnysideWebsiteImg from '../assets/image2.png'
+import budgetappImg from '../assets/image3.png'
+import covid19TrackerImg from '../assets/image4.png'
+import counterappImg from '../assets/image5.png'
+import employeePortalImg from '../assets/image6.png'
+import taskListImg from '../assets/image7.png'
 
 const ProjectData = [
+   {
+      title: "Portfolio Website",
+      description: "A portfolio website about me, my education, skills and experience",
+      codeLink: "https://github.com/TinyikoNdlovu/CodetribePortfolioWebsite.git",
+      projectLink: "https://tinyikondlovu.github.io/CodetribePortfolioWebsite/",
+      imageUrl: portfolioWebsiteImg,
+      tag: "Reactjs",
+   },
     {
-        title: "Frontend Mentor - Sunnyside agency landing page",
-        description: "A landing page running on Web",
-        codeLink: "https://github.com/TinyikoNdlovu/techdevcodeweb.github.io.git",
+        title: "Sunnyside agency landing page",
+        description: "A landing page running on the Web",
+        codeLink: "https://github.com/TinyikoNdlovu/Budget-App.git",
         projectLink: "https://tinyikondlovu.github.io/techdevcodeweb.github.io/",
         imageUrl: sunnysideWebsiteImg,
         tag: "HTML",
@@ -19,7 +29,7 @@ const ProjectData = [
         title: "Budget App",
         description: "A budget app running on the Web",
         codeLink: "https://github.com/TinyikoNdlovu/Budget-App.git",
-        projectLink: "",
+        projectLink: "https://budget-app-e4edd.web.app",
         imageUrl: budgetappImg,
         tag: "Reactjs",
      },
@@ -28,19 +38,37 @@ const ProjectData = [
         title: "Covid-19-Tracker",
         description: "A tracker app running on the Web",
         codeLink: "https://github.com/TinyikoNdlovu/Covid-19-Tracker.git",
-        projectLink: "",
+        projectLink: "https://tinyikondlovu.github.io/Covid-19-Tracker/",
         imageUrl: covid19TrackerImg,
         tag: "Reactjs",
      },
 
      {
+      title: "Counter-App",
+      description: "A counter app to increment and decrement numbers",
+      codeLink: "https://github.com/TinyikoNdlovu/Counter-App.git",
+      projectLink: "https://tinyikondlovu.github.io/Counter-App/",
+      imageUrl: counterappImg,
+      tag: "Reactjs",
+   },
+
+     {
         title: "Employee Portal",
         description: "A CRUD App for Employees running on the Web",
         codeLink: "https://github.com/TinyikoNdlovu/Employee-Portal.git",
-        projectLink: "",
+        projectLink: "https://employee-portal-cadf0.web.app",
         imageUrl: employeePortalImg,
         tag: "Reactjs",
      },
+
+     {
+      title: "User Task Checklist",
+      description: "A web app to add user's Tasks",
+      codeLink: "https://github.com/TinyikoNdlovu/user-task-checklist.git",
+      projectLink: "",
+      imageUrl: taskListImg,
+      tag: "Reactjs",
+   },
 ]
    
 
